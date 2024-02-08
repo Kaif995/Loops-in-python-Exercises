97c8ad7fd48688cd3653c7d3e0daa699b5af5cdb
Mohammed Kaif Shamim <kaifshamim995@gmail.com>
2024-02-08 23:43:29 +0500

    1st Commit

--- Ex9.py
+++ Ex9.py
@@ -1,6 +1,6 @@
 list=(1, 4, 9, 16, 25, 36, 49, 64, 81,100,49)
-x=100
-# x=int(input("Enter any number to find in Above Tuple :"))
+#x=100
+x=int(input("Enter any number to Check in Above Tuple :"))
 index=0
 for el in list:
     if(el == x):
